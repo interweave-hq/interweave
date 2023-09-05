b0c20a0dd2721e6229f180144e9db716a5adff98
Mike Carbone <mfcbone2@gmail.com>
2023-09-05 18:11:26 -0400

    :rocket: Adds sync options

--- src/sync.ts
+++ src/sync.ts
@@ -1,9 +1,14 @@
 import { type InterfaceConfiguration } from "./interfaces";
 import { validateConfiguration } from "./validateConfig";
 
-const API_URL = "https://api.interwv.com/api/v1/projects";
-const BUILD_INTERFACE_URL = ({ projectId }: { projectId: string }) =>
-	`${API_URL}/${projectId}/interfaces`;
+const API_URL = "https://api.interwv.com";
+const BUILD_INTERFACE_URL = ({
+	projectId,
+	apiUrl = API_URL,
+}: {
+	projectId: string;
+	apiUrl?: string;
+}) => `${apiUrl}/api/v1/projects/${projectId}/interfaces`;
 
 /**
  * id - Project ID
@@ -33,9 +38,16 @@ export async function buildInterface(
 	 * token - Project API Token from Interweave
 	 * { id, token }
 	 */
-	project: BuildInterfaceProjectProps
+	project: BuildInterfaceProjectProps,
+	/**
+	 * Additional options
+	 */
+	options?: {
+		apiUrl?: string;
+	}
 ) {
 	const { id, token } = project;
+	const apiUrl = options?.apiUrl;
 
 	// Validate configuration
 	validateConfiguration(config);
@@ -44,16 +56,19 @@ export async function buildInterface(
 	try {
 		console.log("\n");
 		console.log(`⌛ Updating interface ${config.key}...`);
-		const res = await fetch(BUILD_INTERFACE_URL({ projectId: id }), {
-			method: "POST",
-			body: JSON.stringify({
-				schema_config: config,
-			}),
-			headers: {
-				"Content-Type": "application/json",
-				authorization: `Bearer ${token}`,
-			},
-		});
+		const res = await fetch(
+			BUILD_INTERFACE_URL({ projectId: id, apiUrl }),
+			{
+				method: "POST",
+				body: JSON.stringify({
+					schema_config: config,
+				}),
+				headers: {
+					"Content-Type": "application/json",
+					authorization: `Bearer ${token}`,
+				},
+			}
+		);
 		const data = await res.json();
 		if (res.status < 399) {
 			console.log(
@@ -71,6 +86,7 @@ export async function buildInterface(
 			if (data.error.technical_message) {
 				console.log(`${config.key} - ${data.error.technical_message}`);
 			}
+			console.log("\n");
 		}
 	} catch (err) {
 		console.log(err);
